2d835273cef6612d9ee5789fe3e60788d3bf6e27
mohammedsaad <mohammed912009@hotmail.com>
2019-10-04 14:05:47 +0200

    Update Controller.java

--- src/main/java/com/north/csvreader/Controller.java
+++ src/main/java/com/north/csvreader/Controller.java
@@ -10,15 +10,8 @@ import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author mohammed
- */
+
+
 public class Controller {
 
     // get file from classpath, resources folder
